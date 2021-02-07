8eda36dcd8b77bf840390ca9a99261827b625423
lanocci <16540010+lanocci@users.noreply.github.com>
2021-02-07 15:47:36 +0900

    implement dijkstra's algorithm

--- data_structure/single_source_shortest_path/README.md
+++ data_structure/single_source_shortest_path/README.md
@@ -13,3 +13,26 @@
 ### Shortest Path Spanning Tree 最短経路木
 
 - グラフGのなかの頂点sからGの全ての頂点に対してパスがある時、sを根としてsからGの全ての頂点への最短経路を包含するGの全域木
+
+### Dijkstra's algorithm
+
+- Graph `G = (V, E)`
+    - `V` is the set of all vertices in the graph
+    - `E` is the set of all edges in the graph
+- The start point: `s`
+- The set of all vertices in the shortest path `S`
+- For each vertex `i`, minimum cost of the path to the `s` is `d[i]`
+- The parent vertex of a vertex `i` in the shortest path tree is `p[i]`
+
+- In each calculation step, choose a vertex and an edge to add to `S`
+
+1. Initialize parameters
+  - `S`: empty set
+  - `d[s]`: 0
+  - For all `i`, except when `i = s`, `d[u] = inf.`
+2. Repeat following calculations until `S = V`
+  - Select the vertex `u` where `d[u]` is the minimum in `V - S` set
+  - Add `u` to the set `S`, and update all vertices which are next to u and belong to `V - S` with following calculation: 
+    - if `d[u] + w(u, v)` (weight between u and v) < `d[v]`
+      - assign `d[u] + w(u, v)` to `d[v]`
+      - assign `v` to `p[v]`
--- data_structure/single_source_shortest_path/src/main.rs
+++ data_structure/single_source_shortest_path/src/main.rs
@@ -1,3 +1,77 @@
+use std::io::*;
+use util::Scanner;
+
 fn main() {
-    println!("Hello, world!");
+    std::thread::Builder::new()
+        .stack_size(1048576)
+        .spawn(solve)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+fn solve() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    let n: usize = sc.read();
+
+    let mut matrix: Vec<Vec<usize>> = vec![vec![usize::MAX; n]; n];
+    
+    for _ in 0..n {
+        let idx: usize = sc.read();
+        let k = sc.read();
+        for _ in 0..k {
+            let v: usize = sc.read();
+            let c: usize = sc.read();
+            matrix[idx][v] = c;
+        }
+    }
+
+    let d = dijkstra(&matrix, 0);
+
+    for i in 0..n {
+        println!("{} {}", i + 1, d[i]);
+    }
+}
+
+#[derive(Clone, PartialEq)]
+enum Status {
+    White,
+    Black,
+}
+
+fn dijkstra(matrix: & Vec<Vec<usize>>, s: usize) -> Vec<usize> {
+    let n = matrix.len();
+    let mut statuses = vec![Status::White; n];
+    let mut d = vec![usize::MAX; n];
+    d[s] = 0;
+    let mut p = vec![usize::MAX; n];
+
+    loop {
+        let mut min_d = usize::MAX;
+        let mut u = usize::MAX;
+        for i in 0..n {
+            if d[i] < min_d && statuses[i] != Status::Black {
+                min_d = d[i];
+                u = i;
+            }
+        }
+
+        if min_d == usize::MAX { break; }
+
+        statuses[u] = Status::Black;
+
+        for v in 0..n {
+            if statuses[v] != Status::Black && matrix[u][v] != usize::MAX {
+                if d[u] + matrix[u][v] < d[v] {
+                    d[v] = d[u] + matrix[u][v];
+                    p[v] = u;
+                }
+            }
+        }
+    }
+    d
+
 }
