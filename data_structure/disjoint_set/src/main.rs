97f1ef037cd2a01da9df6b25b5379d8d5dd04b15
lanocci <16540010+lanocci@users.noreply.github.com>
2021-02-09 23:14:46 +0900

    add documents

--- data_structure/disjoint_set/src/main.rs
+++ data_structure/disjoint_set/src/main.rs
@@ -37,12 +37,16 @@ fn solve() {
     }
 }
 
+/// contains metadata for a disjoint set
+/// rank represents hight of each set represented as tree
+/// p represents index of each node's parent node
 struct DisjointSet {
     rank: Vec<usize>,
     p: Vec<usize>,
 }
 
 
+/// ## DisjointSet implementation with ranks and path compression
 impl DisjointSet {
     fn new(n: usize) -> DisjointSet {
         DisjointSet {
@@ -51,12 +55,16 @@ impl DisjointSet {
         }
     }
     
+    /// unite two disjointsets by linking each representative node with link method
     fn unite(&mut self, this: usize, that: usize) {
         let this = self.find_set(this);
         let that = self.find_set(that);
         self.link(this, that);
     }
 
+    /// link representative nodes
+    /// new representative node will be the one that holds higher rank
+    /// if the both nodes hold the same rank, choose either of them and increment rank by 1
     fn link(&mut self, this: usize, that: usize) {
         if self.rank[this] > self.rank[that] {
             self.p[that] = this;
@@ -68,10 +76,15 @@ impl DisjointSet {
         }
     }
 
+    /// compare representative nodes of the sets where x and y node belongs respectively to.
     fn same(&mut self, this: usize, that: usize) -> bool {
         self.find_set(this) == self.find_set(that)
     }
 
+    /// return the representative node in a set where the x node belongs to; by following parent nodes. 
+    /// (each node's  parent is initialized with self)
+    /// at the same time perform path compression
+    /// path compression makes it more efficient to perform find_set next time
     fn find_set(&mut self, x: usize) -> usize {
         if self.p[x] != x {
             self.p[x] = self.find_set(self.p[x]);
