1faec241a0681de9d10234469e01cdef98ceebc9
lanocci <16540010+lanocci@users.noreply.github.com>
2021-02-11 12:02:28 +0900

    refactor kd tree

--- data_structure/kd_tree/src/main.rs
+++ data_structure/kd_tree/src/main.rs
@@ -24,18 +24,16 @@ fn solve() {
         points.push(Point{id: i, x, y,});
     }
 
-    let mut nodes: Vec<Node> = Vec::new();
-    make_bidimensional_tree(0, n, 0, &mut points, &mut nodes);
+    let mut bt = BidimensionalTree::new(points);
 
     let q: usize = sc.read();
 
     for _ in 0..q {
-        let mut answer = Vec::new();
         let sx: usize = sc.read();
         let tx: usize = sc.read();
         let sy: usize = sc.read();
         let ty: usize = sc.read();
-        find(0, sx, tx, sy, ty, 0, &points, &nodes, &mut answer);
+        let mut answer = bt.find(sx, tx, sy, ty);
         answer.sort_by_key(|a| a.id);
         for a in answer.iter() {
             println!("{}", a.id);
@@ -58,48 +56,74 @@ struct Node {
     pub right: Option<usize>,
 }
 
-fn make_bidimensional_tree(l: usize, r: usize, depth: usize, points: &mut Vec<Point>, nodes: &mut Vec<Node>) -> Option<usize> {
-    if !(l < r) { return None; }
-    let mid = (l + r) / 2;
-    if depth % 2 == 0 {
-        points[l..r].sort_by_key(|p| p.x);
-    } else {
-        points[l..r].sort_by_key(|p| p.y);
-    }
-    let t = nodes.len();
-    nodes.push(Node{location: mid, left: None, right: None});
-    nodes[t].left = make_bidimensional_tree(l, mid, depth + 1, points, nodes);
-    nodes[t].right = make_bidimensional_tree(mid + 1, r, depth + 1, points, nodes);
-    Some(t)
+struct BidimensionalTree {
+    points: Vec<Point>,
+    nodes: Vec<Node>,
 }
 
-fn find(v: usize, sx: usize, tx: usize, sy: usize, ty: usize, depth: usize, points: &Vec<Point>, nodes: &Vec<Node>, answer: &mut Vec<Point>) {
-    let x = points[nodes[v].location].x;
-    let y = points[nodes[v].location].y;
+impl BidimensionalTree {
 
-    if sx <= x && sy <= y && x <= tx && y <= ty { answer.push(points[nodes[v].location].clone()); }
+    fn new(points: Vec<Point>) -> BidimensionalTree {
+        let mut bt = BidimensionalTree {
+            points,
+            nodes: Vec::new(),
+        };
+        bt.make_nodes(0, bt.points.len(), 0);
+        bt
+    }
 
-    if depth % 2 == 0 {
-        if let Some(l) = nodes[v].left {
-            if sx <= x {
-                find(l, sx, tx, sy, ty, depth + 1, points, nodes, answer);
-            }
+    fn make_nodes(&mut self, l: usize, r: usize, depth: usize) -> Option<usize> {
+        if !(l < r) { return None; }
+        let mid  = (l + r) / 2;
+        if depth % 2 == 0 {
+            self.points[l..r].sort_by_key(|p| p.x);
+        } else {
+            self.points[l..r].sort_by_key(|p| p.y);
         }
-        if let Some(r) = nodes[v].right {
-            if x <= tx {
-                find(r, sx, tx, sy, ty, depth + 1, points, nodes, answer);
-            }
+        let t = self.nodes.len();
+        self.nodes.push(Node{location: mid, left: None, right: None});
+        self.nodes[t].left = self.make_nodes(l, mid, depth + 1);
+        self.nodes[t].right = self.make_nodes(mid + 1, r, depth + 1);
+        Some(t)
+    }
+
+    fn find(&mut self, sx: usize, tx: usize, sy: usize, ty: usize) -> Vec<Point>{
+        let mut ans: Vec<Point> = Vec::new();
+        self.find_rec(0, sx, tx, sy, ty, 0, &mut ans);
+        ans
+    }
+
+    fn find_rec(&mut self, v: usize, sx: usize, tx: usize, sy: usize, ty: usize, depth: usize, ans: &mut Vec<Point>) {
+        let x = self.points[self.nodes[v].location].x;
+        let y = self.points[self.nodes[v].location].y;
+
+        if sx <= x && x <= tx && sy <= y && y <= ty {
+            ans.push(self.points[self.nodes[v].location].clone());
         }
-    } else {
-        if let Some(l) = nodes[v].left {
-            if sy <= y {
-                find(l, sx, tx, sy, ty, depth + 1, points, nodes, answer);
+
+        if depth % 2 == 0 {
+            if let Some(l) = self.nodes[v].left {
+                if sx <= x {
+                    self.find_rec(l, sx, tx, sy, ty, depth + 1, ans);
+                }
             }
-        }
-        if let Some(r) = nodes[v].right {
-            if y <= ty {
-                find(r, sx, tx, sy, ty, depth + 1, points, nodes, answer);
+            if let Some(r) = self.nodes[v].right {
+                if x <= tx {
+                    self.find_rec(r, sx, tx, sy, ty, depth + 1, ans);
+                }
+            }
+        } else {
+            if let Some(l) = self.nodes[v].left {
+                if sy <= y {
+                    self.find_rec(l, sx, tx, sy, ty, depth + 1, ans);
+                }
+            }
+            if let Some(r) = self.nodes[v].right {
+                if y <= ty {
+                    self.find_rec(r, sx, tx, sy, ty, depth + 1, ans);
+                }
             }
         }
     }
+
 }
