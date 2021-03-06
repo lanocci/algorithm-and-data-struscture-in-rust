ccada3e45e587730df77bb9e634e2c9aef7ad20b
lanocci <16540010+lanocci@users.noreply.github.com>
2021-03-06 14:42:38 +0900

    refactor puzzle

--- strategy/eight_puzzle/src/main.rs
+++ strategy/eight_puzzle/src/main.rs
@@ -1,7 +1,6 @@
 use std::io::*;
-use std::collections::{VecDeque, HashSet};
-use util::Scanner;
-use puzzle::{Puzzle, Direction};
+use util::{Scanner, Joinable};
+use puzzle::Puzzle;
 
 fn main() {
     std::thread::Builder::new()
@@ -24,50 +23,7 @@ fn solve() {
         }
         problem
     };
-    println!("{}", eight_puzzle(&problem).len());
-}
-
-fn eight_puzzle(problem: &Vec<usize>) -> Vec<String> {
-    let mut q: VecDeque<Puzzle> = VecDeque::new();
-    let init = Puzzle::new(problem);
-    q.push_back(init.clone());
-
-    let directions = vec![Direction::Up, Direction::Left, Direction::Down, Direction::Right];
-
-    let mut v: HashSet<Puzzle> = HashSet::new();
-    v.insert(init);
-
-    while let Some(p) = q.pop_front() {
-        if is_target(&p) { return p.path; }
-        let space_location_x = p.space_idx as i32 / p.row_count;
-        let space_location_y = p.space_idx as i32 % p.row_count;
-
-        for d in directions.iter() {
-            let target_tile_location_x = space_location_x + d.vertical_move();
-            let target_tile_location_y = space_location_y + d.horizontal_move();
-            if target_tile_location_x < 0 || target_tile_location_y < 0 || target_tile_location_x >= p.row_count || target_tile_location_y >= p.row_count { continue; }
-            let mut u = p.clone();
-            let new_space_idx = (target_tile_location_x * p.row_count + target_tile_location_y) as usize;
-            let old_tile = u.f[new_space_idx];
-            u.f[p.space_idx] = old_tile;
-            u.f[new_space_idx] = 0;
-            u.space_idx = new_space_idx;
-            match v.get(&u) {
-                None => {
-                    v.insert(u.clone());
-                    u.path.push(d.to_string());
-                    q.push_back(u);
-                },
-                Some(_) => {}
-            }
-        }
-    }
-    unreachable!()
-}
-
-fn is_target(p: &Puzzle) -> bool {
-    for i in 0..(p.f.len()) {
-        if p.f[i] != (i + 1) % 9 { return false; }
-    }
-    true
+    let puzzle = Puzzle::new(&problem);
+    let ans = puzzle.solve_naive();
+    println!("{}", ans.get_path().len());
 }
--- strategy/puzzle/src/lib.rs
+++ strategy/puzzle/src/lib.rs
@@ -1,5 +1,7 @@
 use std::cmp::Ordering;
+use std::collections::{VecDeque, HashSet};
 use std::fmt;
+use util::Joinable;
 
 #[derive(Eq, Clone, Hash)]
 pub enum Puzzle {
@@ -37,6 +39,134 @@ impl Puzzle {
         match self {
             Self::EightPuzzle{f,..} => f.clone(),
             Self::FifteenPuzzle{f,..} => f.clone(),
+        }
+    }
+    
+    pub fn get_space_idx(&self) -> usize {
+        match self {
+            Self::EightPuzzle{space_idx, ..} => *space_idx,
+            Self::FifteenPuzzle{space_idx, ..} => *space_idx,
+        }
+    }
+    pub fn get_path(&self) -> Vec<String> {
+        match self {
+            Self::EightPuzzle{path, ..} => path.clone(),
+            Self::FifteenPuzzle{path, ..} => path.clone(),
+        }
+    }
+
+    fn push_path(&mut self, x: String) {
+        match self {
+            Self::EightPuzzle{path, ..} => path.push(x),
+            Self::FifteenPuzzle{path, ..} => path.push(x),
+        }
+    }
+
+    fn set_space_idx(&mut self, x: usize) {
+        match self {
+            Self::EightPuzzle{space_idx, ..} => {
+                if space_idx <= &mut 9 {
+                    *space_idx = x;
+                } else {
+                    panic!();
+                }
+            },
+            Self::FifteenPuzzle{space_idx, ..} => {
+                if space_idx <= &mut 16 {
+                    *space_idx = x;
+                } else {
+                    panic!();
+                }
+            },
+        }
+    }
+    fn set_new_tiles(&mut self, tiles: &Vec<usize>) {
+        match self {
+            Self::EightPuzzle{f, ..} => {
+                if tiles.len() == 9 {
+                    *f = tiles.clone();
+                } else {
+                    panic!();
+                }
+            },
+            Self::FifteenPuzzle{f, ..} => {
+                if tiles.len() == 16 {
+                    *f = tiles.clone();
+                } else {
+                    panic!();
+                }
+            },
+        }
+    }
+
+    pub fn solve_naive(&self) -> Self {
+        match self {
+            p @ Self::EightPuzzle {..} => {
+                const ROW_COUNT: i32 = 3;
+                let mut q: VecDeque<Puzzle> = VecDeque::new();
+                q.push_back(p.clone());
+
+                let directions = vec![Direction::Up, Direction::Left, Direction::Down, Direction::Right];
+            
+                let mut v: HashSet<Puzzle> = HashSet::new();
+                v.insert(p.clone());
+            
+                while let Some(pzl) = q.pop_front() {
+                    if pzl.is_target() { return pzl; }
+                    let space_location_x = pzl.get_space_idx() / ROW_COUNT as usize;
+                    let space_location_y = pzl.get_space_idx() % ROW_COUNT as usize;
+            
+                    for d in directions.iter() {
+                        let target_tile_location_x = space_location_x as i32 + d.vertical_move();
+                        let target_tile_location_y = space_location_y as i32 + d.horizontal_move();
+                        if target_tile_location_x < 0 || target_tile_location_y < 0 || target_tile_location_x >= ROW_COUNT || target_tile_location_y >= ROW_COUNT { continue; }
+                        let mut u = pzl.clone();
+                        let new_space_idx = (target_tile_location_x * ROW_COUNT + target_tile_location_y) as usize;
+                        let mut new_tiles = u.get_tiles();
+                        let old_tile = new_tiles[new_space_idx];
+                        new_tiles[pzl.get_space_idx()] = old_tile;
+                        new_tiles[new_space_idx] = 0;
+                        u.set_space_idx(new_space_idx);
+                        u.set_new_tiles(&new_tiles);
+                        match v.get(&u) {
+                            None => {
+                                v.insert(u.clone());
+                                u.push_path(d.to_string());
+                                q.push_back(u);
+                            },
+                            Some(_) => {}
+                        }
+                    }
+                }
+                unreachable!()
+            },
+            p @ Self::FifteenPuzzle {..} => {
+                p.clone()
+            }
+        }
+    }
+
+    fn is_target(&self) -> bool {
+        match self {
+            Self::EightPuzzle {f, ..} => {
+                for i in 0..9 {
+                    if f[i] !=  (i + 1) % 9 { return false; }
+                }
+                true
+            },
+            Self::FifteenPuzzle {f, ..} => {
+                for i in 0..16 {
+                    if f[i] != (i + 1) % 16 { return false; }
+                }
+                true
+            }
+        }
+    }
+
+    fn row_count(&self) -> i32 {
+        match self {
+            Self::EightPuzzle {..} => 3,
+            Self::FifteenPuzzle {..} => 4,
         }
     }
 }
