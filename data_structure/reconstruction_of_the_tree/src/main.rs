8b565ca2899ee063c55ba4183609ce08d6a8b3aa
lanocci <16540010+lanocci@users.noreply.github.com>
2021-03-15 19:30:38 +0900

    implement reconstruction of the tree

---
+++ data_structure/reconstruction_of_the_tree/src/main.rs
@@ -0,0 +1,37 @@
+use std::io::*;
+use util::Scanner;
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(1048576)
+        .spawn(solve)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+fn solve() {
+    let cin = stdin();
+    let cin = cin.lock();
+    let mut sc = Scanner::new(cin);
+
+    let n = sc.read();
+    let preorder: Vec<usize> = sc.vec(n);
+    let inorder: Vec<usize> = sc.vec(n);
+
+    let mut postorder: Vec<usize> = Vec::new();
+    let mut position: usize = 0;
+    reconstruction(&mut postorder, &preorder, &inorder, &mut position, 0, n);
+
+    println!("{}", postorder.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" "));
+}
+
+fn reconstruction(postorder: &mut Vec<usize>, preorder: &Vec<usize>, inorder: &Vec<usize>, pos: &mut usize, l: usize, r: usize) {
+    if l >= r { return; }
+    let root = preorder[*pos];
+    *pos += 1;
+    let m = inorder.iter().enumerate().find(|(_, x)| x == &&root).unwrap().0;
+    reconstruction(postorder, preorder, inorder, pos, l, m);
+    reconstruction(postorder, preorder, inorder, pos, m + 1, r);
+    postorder.push(root);
+}
